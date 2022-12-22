1758882c30ea8f64928658721b60ccc0e2ca2204
SantiagoGallego1 <santiago.gallego.mt@usa.edu.co>
2022-12-22 18:43:59 -0500

    Add

--- routes/categoriasRouters.js
+++ routes/categoriasRouters.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authMidd = require("../middleware/authMidd");
 const categoriasController = require("../controllers/categoriasController");
 
-router.get("/", categoriasController.obtenerCategoriaHome );
+router.get("/home", categoriasController.obtenerCategoriaHome );
 router.get("/", authMidd, categoriasController.obtenerCategoria   );
 router.get("/:id", authMidd, categoriasController.obtenerCategoriaId   );
 router.post("/", authMidd,categoriasController.crearCategoria );
